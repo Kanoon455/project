dbd4865440047dec8d5f39f9aa2038843bed983b
Nuttapon Sudjaritbun <kanoon455@Nuttapons-MacBook-Pro.local>
2022-05-29 03:08:57 +0700

    adminweb

---
+++ adminweb/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function home() {
+    return (
+        <div className="home">
+           <div className="container-fluid">
+                <h1>Welcome to home page.</h1>
+           </div>
+        </div>
+    );
+}
+
+export default home;
