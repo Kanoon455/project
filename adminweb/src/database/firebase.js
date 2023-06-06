d1f7684f7bf7ee6db9e6e74d17808c172ee1e1ff
Kanoon455 <106450514+Kanoon455@users.noreply.github.com>
2023-06-06 14:07:17 +0700

    Update firebase.js
    hide password

--- adminweb/src/database/firebase.js
+++ adminweb/src/database/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXcp9PradCLCEaCHdb3AJRqO2RbpCAxK8",
-  authDomain: "register-admin-a7afd.firebaseapp.com",
-  projectId: "register-admin-a7afd",
-  storageBucket: "register-admin-a7afd.appspot.com",
-  messagingSenderId: "1004169494666",
-  appId: "1:1004169494666:web:f6041037b026cbe390ed73",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
